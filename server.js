f247b2c95998f27c6244eba8fef368eab5b3687b
Walter Mattingly <waltdakind@gmail.com>
2016-05-16 20:07:04 -0400

    added sitemap.xml for seo req

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
+//seo stuff
+var sitemap = require('sitemap.xml');
+app.use(sitemap(__dirname + 'public/sitemap.xml'));
 // Load static files
 app.use(express.static('./test/public'));
 
